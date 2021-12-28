ce53e0c99bba239e3cd4eda03bc34293734df1f5
D3NCyan <edwarddoltons@gmail.com>
2021-12-29 07:41:37 +0800

    use route login and index

--- Test Script - Back End/src/routes/index.js
+++ Test Script - Back End/src/routes/index.js
@@ -1,7 +1,13 @@
 const express = require('express')
 // controller
 const { auth } = require('../middlewares/auth')
+const { getData } = require('../controllers')
+const { login } = require('../controllers/auth')
 
 const router = express.Router()
 
-router.get('/', auth, )
+router.get('/', auth, getData)
+
+router.post('/login', login)
+
+module.exports = router
