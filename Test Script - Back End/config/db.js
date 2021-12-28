84546004dfde7d6cc5089ad8228362cd1bf0d6d8
D3NCyan <edwarddoltons@gmail.com>
2021-12-29 07:22:27 +0800

    create pool and configuration

---
+++ Test Script - Back End/config/db.js
@@ -0,0 +1,10 @@
+const Pool = require('pg')
+const db = new Pool({
+    user: 'me',
+    host: process.env.HOST || 'localhost',
+    database: 'pt_fan_api',
+    password: process.env.PASS_USER,
+    port: 5432,
+})
+
+module.exports = db
