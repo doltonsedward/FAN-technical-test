91a299f894a6ccfa5649ec98f090e62b81076758
D3NCyan <edwarddoltons@gmail.com>
2021-12-29 07:22:11 +0800

    add api route

--- Test Script - Back End/index.js
+++ Test Script - Back End/index.js
@@ -1,8 +1,12 @@
 require('dotenv')
 const express = require('express')
-const app = express()
 
+const app = express()
+const router = require('./src/routes')
 const port = 5000 | process.env.PORT
-app.use(express.json)
+
+app.use(express.json())
+
+app.use('/api/v1', router)
 
 app.listen(port, () => console.log('Server running on port: ', port))
