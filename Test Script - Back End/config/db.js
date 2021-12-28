7a9999bf77371712e353d58e38932d7e0e5eb57d
D3NCyan <edwarddoltons@gmail.com>
2021-12-29 07:40:50 +0800

    fix Pool and add password in string

--- Test Script - Back End/config/db.js
+++ Test Script - Back End/config/db.js
@@ -1,9 +1,9 @@
-const Pool = require('pg')
+const Pool = require('pg').Pool
 const db = new Pool({
     user: 'me',
     host: process.env.HOST || 'localhost',
     database: 'pt_fan_api',
-    password: process.env.PASS_USER,
+    password: process.env.PASS_USER || 'Adatongg_05',
     port: 5432,
 })
 
