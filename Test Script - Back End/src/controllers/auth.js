1f7392336b3bb52b5ab4fba04c99bbcf8d7b70b5
D3NCyan <edwarddoltons@gmail.com>
2021-12-29 08:19:13 +0800

    done login feature

--- Test Script - Back End/src/controllers/auth.js
+++ Test Script - Back End/src/controllers/auth.js
@@ -1,9 +1,36 @@
 const db = require("../../config/db")
 
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
 exports.login = (req, res) => {
-    db.query('SELECT * FROM users', (error, result) => {
+    const { email } = req.body
+    
+    db.query(`SELECT * FROM users WHERE email = $1`, [email], async (error, result) => {
         if (error) throw error
 
-        res.status(200).send(result.rows)
+        if (!result.rows.length) {
+            return res.status(404).send({
+                status: "error",
+                message: "Email or password are incorrect"
+            })
+        }
+
+        const isPassValid = await bcrypt.compare(req.body.password, result.rows[0].password)
+        if (!isPassValid) {
+            return res.status(404).send({
+                status: "error",
+                message: "Email or password are incorrect"
+            })
+        }
+
+        const { id } = result.rows[0]
+
+        const token = jwt.sign({ id }, process.env.TOKEN_KEY, { expiresIn: '1d' })
+
+        res.status(200).send({
+            status: 'success',
+            token
+        })
     })
 }
