3ef554c1f4b279ca5b8560742c925cf50822a761
D3NCyan <edwarddoltons@gmail.com>
2021-12-29 08:19:49 +0800

    fix use token key name

--- Test Script - Back End/src/middlewares/auth.js
+++ Test Script - Back End/src/middlewares/auth.js
@@ -7,7 +7,7 @@ exports.auth = (req, res, next) => {
     if (!token) return res.status(400).send({ message: "Access Denied!, login first" })
 
     try {
-        const verified = jwt.verify(token, process.env.TKEY)
+        const verified = jwt.verify(token, process.env.TOKEN_KEY)
         req.user = verified
         next()
     } catch (error) {
